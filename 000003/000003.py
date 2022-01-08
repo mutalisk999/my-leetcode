33bb32494a4281f55d5c88224a87439bea71f2bd
mutalisk999 <tangjian999999999@gmail.com>
2022-01-09 00:28:38 +0800

    3 - longest-substring-without-repeating-characters

--- 000003/000003.py
+++ 000003/000003.py
@@ -1,28 +1,20 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         idxS, idxE, substrlenMax, strlen = 0, 0, 0, len(s)
+        d = dict()
 
         while True:
-            d = dict()
-            while True:
-                if idxE >= strlen:
-                    break
-                if s[idxE] not in d:
-                    d[s[idxE]] = idxE
-                else:
-                    break
-                idxE = idxE + 1
-
-            l = idxE - idxS
-            if l > substrlenMax:
-                substrlenMax = l
-
             if idxE >= strlen:
-                return substrlenMax
+                return idxE - idxS if idxE - idxS > substrlenMax else substrlenMax
 
-            if s[idxE] in d:
+            if s[idxE] not in d:
+                d[s[idxE]] = idxE
+                idxE = idxE + 1
+            else:
+                substrlenMax = idxE - idxS if idxE - idxS > substrlenMax else substrlenMax
                 idxS = d[s[idxE]] + 1
                 idxE = idxS
+                d = dict()
 
 
 if __name__ == "__main__":
