43bff49777f6cf3b79225b011707fdcb23241027
mutalisk999 <tangjian999999999@gmail.com>
2022-01-09 12:48:03 +0800

    4 - median-of-two-sorted-arrays

--- 000004/000004.py
+++ 000004/000004.py
@@ -4,25 +4,44 @@ from typing import List
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         idx1, idx2, len1, len2 = 0, 0, len(nums1), len(nums2)
-        merged = list()
-        while True:
-            if idx1 >= len1:
-                merged.extend(nums2[idx2:])
-                break
-            if idx2 >= len2:
-                merged.extend(nums1[idx1:])
-                break
-            if nums1[idx1] < nums2[idx2]:
-                merged.append(nums1[idx1])
-                idx1 = idx1 + 1
-            else:
-                merged.append(nums2[idx2])
-                idx2 = idx2 + 1
-        mlen = len(merged)
-        if mlen & 0x1 == 0x1:
-            return float(merged[mlen//2])
+        mid1, mid2, cnt = 0, 0, 0
+        mid1v = 0
+
+        lentotal = len1 + len2
+        if lentotal & 0x1 == 0x1:
+            mid1 = lentotal // 2
         else:
-            return float((merged[mlen//2] + merged[mlen//2 - 1]) / 2)
+            mid2 = lentotal // 2
+            mid1 = mid2 - 1
+
+        while True:
+            v = 0
+
+            if idx1 >= len1 or idx2 >= len2:
+                if idx1 >= len1:
+                    v = nums2[idx2]
+                    idx2 = idx2 + 1
+                elif idx2 >= len2:
+                    v = nums1[idx1]
+                    idx1 = idx1 + 1
+            else:
+                if nums1[idx1] >= nums2[idx2]:
+                    v = nums2[idx2]
+                    idx2 = idx2 + 1
+                else:
+                    v = nums1[idx1]
+                    idx1 = idx1 + 1
+
+            if lentotal & 0x1 == 0x1:
+                if cnt == mid1:
+                    return float(v)
+            else:
+                if cnt == mid1:
+                    mid1v = v
+                elif cnt == mid2:
+                    return float((mid1v + v) / 2)
+
+            cnt = cnt + 1
 
 
 if __name__ == "__main__":
