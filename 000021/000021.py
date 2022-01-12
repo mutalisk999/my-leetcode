e6e4c430c14de41701c6ce4dd53d9e2b7c86df69
mutalisk999 <tangjian999999999@gmail.com>
2022-01-12 16:01:39 +0800

    21 - merge-two-sorted-lists

--- 000021/000021.py
+++ 000021/000021.py
@@ -5,8 +5,8 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
-        
+
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if list1 is None:
@@ -41,8 +41,8 @@ class Solution:
                         n.next = t
                         head2 = head2.next
                     n = n.next
-                
-    
+
+
 def NodeToList(l: ListNode) -> list:
     ret = list()
     while l is not None:
@@ -67,12 +67,12 @@ if __name__ == "__main__":
     sol = Solution()
     list1 = [1, 2, 4]
     list2 = [1, 3, 4]
-    print(NodeToList(sol.mergeTwoLists(ListToNode(list1), ListToNode(list2))))
-
+    print(NodeToList(sol.mergeTwoLists(ListToNode(list1), ListToNode(list2))))  # type: ignore
+    
     list1 = []
     list2 = []
-    print(NodeToList(sol.mergeTwoLists(ListToNode(list1), ListToNode(list2))))
-
+    print(NodeToList(sol.mergeTwoLists(ListToNode(list1), ListToNode(list2))))  # type: ignore
+    
     list1 = []
     list2 = [0]
-    print(NodeToList(sol.mergeTwoLists(ListToNode(list1), ListToNode(list2))))
+    print(NodeToList(sol.mergeTwoLists(ListToNode(list1), ListToNode(list2))))  # type: ignore
