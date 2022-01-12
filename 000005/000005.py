dbe55b0c3a2f642d0103d9c0cc6a7c414292d09a
mutalisk999 <tangjian999999999@gmail.com>
2022-01-12 11:24:35 +0800

    5 - longest-palindromic-substring

--- 000005/000005.py
+++ 000005/000005.py
@@ -4,9 +4,20 @@ class Solution:
         for i in range(sl):
             for j in range(0, i+1):
                 t = s[j: j+sl-i]
+                
+                #
                 if t == t[::-1]:
                     return t
-
+                
+                #
+                # lt, f = len(t), True
+                # for m in range(lt // 2):
+                #     if t[m] != t[lt-1-m]:
+                #         f = False
+                #         break
+                # if f:
+                #     return t
+                    
 
 if __name__ == "__main__":
     sol = Solution()
